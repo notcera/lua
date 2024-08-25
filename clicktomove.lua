6f1578ff8a594b7b0dd00b4f17317f76ea7a103b
notcera <nabicantdev@gmail.com>
2024-08-25 12:36:19 +0800

    Update clicktomove.lua

--- clicktomove.lua
+++ clicktomove.lua
@@ -1,4 +1,3 @@
--- click tp
 loadstring(game:HttpGet("https://raw.githubusercontent.com/notcera/lua/main/global.lua"))()
 
 local Player = Players.LocalPlayer
@@ -6,19 +5,19 @@ local Mouse = Player:GetMouse()
 
 function PathFind(Position)
     local Character = GetCharacter()
-	local Humanoid = GetHumanoid()
+    local Humanoid = GetHumanoid()
     if not Character or not Humanoid then return end
 
     local Extents = Character:GetExtentsSize()
     AgentRadius = 1 * 0.5 * math.sqrt(Extents.X * Extents.X + Extents.Z * Extents.Z)
     AgentHeight = 1 * Extents.Y
-
+    
     local Path = PathfindingService:CreatePath({WaypointSpacing = 8, AgentCanJump = true, AgentRadius = AgentRadius, AgentHeight = AgentHeight, AgentCanClimb = true})
     local Humanoid = Character:WaitForChild("Humanoid")
 
     local Success, ErrorMessage = pcall(function()
-		Path:ComputeAsync(Character:GetPivot().p, Position)
-	end)
+        Path:ComputeAsync(Character:GetPivot().p, Position)
+    end)
 
     if Path.Status == Enum.PathStatus.Success then
         local Waypoints = Path:GetWaypoints()
@@ -27,9 +26,9 @@ function PathFind(Position)
             if i == 1 then continue end
             Humanoid:MoveTo(v.Position)
         end
-	else
-		warn("Path not computed!", ErrorMessage)
-	end
+    else
+        warn("Path not computed!", ErrorMessage)
+    end
 end
 
 if Button1DownSignal then Button1DownSignal:Disconnect() end
